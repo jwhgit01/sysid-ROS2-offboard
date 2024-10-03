f8930cc9b50922201e370e3df92aae54404d42d9
jwhgit01 <jeremy@hopwoodclan.org>
2024-10-03 15:54:30 -0400

    minor

--- startup.bash
+++ startup.bash
@@ -2,4 +2,4 @@
 
 source /opt/ros/humble/setup.bash
 source /ros_ws/install/setup.bash
-ros2 run velocity_mode velocity_mode & MicroXRCEAgent serial --dev /dev/ttyUSB0 -b 921600
+ros2 run velocity_command velocity_command & MicroXRCEAgent serial --dev /dev/ttyUSB0 -b 921600
